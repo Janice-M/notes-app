4775cceac296b0059f2bdee0da1d31e4920de40c
Janice-M <janiceink001@gmail.com>
2020-11-16 22:21:26 +0300

    sequelize

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+const 
 app.get('/', (req,res)=> res.send('Notes App'));
 
 app.listen(port, () => console.log(`notes-app listening on port ${3000}!`));
