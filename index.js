2d5eba2ff4594c84545ea4c8eae74764a9755ffd
Janice-M <janiceink001@gmail.com>
2020-11-16 22:22:04 +0300

    sequelize

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+const Sequelize = require ('sequelize');
 const 
 app.get('/', (req,res)=> res.send('Notes App'));
 
