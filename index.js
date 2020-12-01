b04c313eeb38da8f134bce625f2ce0bc15b599e0
Janice-M <janiceink001@gmail.com>
2020-12-01 22:54:28 +0300

    notes model

--- index.js
+++ index.js
@@ -23,6 +23,23 @@ app.listen(port, () => console.log(`notes-app listening on port ${3000}!`));
 
 const Note = sequelize.define('notes', { note: Sequelize.TEXT, tag: Sequelize.STRING });
 sequelize.sync({ force: true })
-  .then(() => {
+    .then(() => {
     console.log(`Database & tables created!`);
-  });
+    });
+
+
+    sequelize.sync({ force: true })
+    .then(() => {
+      console.log(`Database & tables created!`);
+  
+      Note.bulkCreate([
+        { note: 'pick up some bread after work', tag: 'shopping' },
+        { note: 'remember to write up meeting notes', tag: 'work' },
+        { note: 'learn how to use node orm', tag: 'work' }
+      ]).then(function() {
+        return Note.findAll();
+      }).then(function(notes) {
+        console.log(notes);
+      });
+    });
+  
