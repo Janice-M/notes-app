1853e517d2f5d9f3d0edcde5264dd6084f984baa
Janice-M <janiceink001@gmail.com>
2020-12-01 22:55:11 +0300

    notes model

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ sequelize.sync({ force: true })
     sequelize.sync({ force: true })
     .then(() => {
         console.log(`Database & tables created!`);
-  
+
         Note.bulkCreate([
         { note: 'pick up some bread after work', tag: 'shopping' },
         { note: 'remember to write up meeting notes', tag: 'work' },
         { note: 'learn how to use node orm', tag: 'work' }
-      ]).then(function() {
+        ]).then(function() {
         return Note.findAll();
       }).then(function(notes) {
         console.log(notes);
